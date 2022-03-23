92a6c267a73071e0d7f94ae02fbc29bd66e02ad2
Ray Lin <ray.lin.tw@protonmail.com>
2022-03-23 21:12:45 +0000

    Improve drag and drop
    Now correctly deletes entire line in preview. Also corrected the behaviour of undo. Now a failed drop will always revert back to the original state.

--- src/editor/codemirror/dnd-extension.ts
+++ src/editor/codemirror/dnd-extension.ts
@@ -1,15 +1,21 @@
 import { debug as dndDebug, setDragContext } from "./dnd";
 import { gutter, GutterMarker } from "@codemirror/gutter";
-import {EditorView} from "@codemirror/view"
+import { EditorView } from "@codemirror/view"
+import { ChangeSet, Transaction } from "@codemirror/state";
+import { debug } from "console";
 
 const handleElement = new class extends GutterMarker {
-  toDOM() { 
-    const markerNode = document.createElement("span") 
-    markerNode.innerText = "⸭" 
+  toDOM() {
+    const markerNode = document.createElement("span")
+    markerNode.innerText = "⸭"
     markerNode.draggable = true
     return markerNode
   }
 }
+
+// Very ugly way to check whether item was successfully 
+let dropped = false;
+let notDroppedUndo: ChangeSet | undefined;
 
 const dndHandleGutter = [
   gutter({
@@ -23,29 +29,48 @@ const dndHandleGutter = [
 
         const line = view.state.doc.lineAt(block.from);
         const lineCode = line.text
-        
+
         //delete original text, leaves blank line
         //atm this means an extra line is added to the file
+        let deleteLineChange = view.state.update(
+          {
+            changes: { from: line.from, to: Math.min(view.state.doc.length, line.to + 1) }
+          }).changes
+        notDroppedUndo = deleteLineChange.invert(view.state.doc);
+
         view.dispatch({
-          changes: {from: line.from, to: line.to}
+          userEvent: "delete-drag-line",
+          changes: deleteLineChange,
+          annotations: [Transaction.addToHistory.of(false)],
         })
 
         dndDebug("dragstart");
 
+        dropped = false;
+
         //magic line to tell dnd.ts to handle the previews and drop
         setDragContext({
           code: lineCode,
-          type: "example", //not sure what this is for
-          id: "pSlug"      //or this
+          type: "example",
+          id: "pSlug",
+          redoToMerge: deleteLineChange,
+          undoToMerge: notDroppedUndo,
+          dropCallback: () => { dropped = true; }
         });
 
         //must return false to let the drop be handled
         return false
       },
-      drop(view, block, event) {
+      dragend(view, block, event) {
         dndDebug("dragend");
-        //reset the dnd.ts draging
+        //reset the dnd.ts dragging
         setDragContext(undefined);
+        if (!dropped) {
+          view.dispatch({
+            changes: notDroppedUndo,
+            annotations: [Transaction.addToHistory.of(false)],
+          })
+        }
         return false
       }
     }
@@ -54,10 +79,10 @@ const dndHandleGutter = [
   // so I have used theme directly
   EditorView.theme({
     ".cm-dnd-gutter": {
-        // Default gutter width is huge 
-        width: "fit-content"
+      // Default gutter width is huge 
+      width: "fit-content"
     }
   })
 ]
 
-export {dndHandleGutter}
+export { dndHandleGutter }
