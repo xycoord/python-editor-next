a0ab2db8b3592dde5042e5034d825d5b5584e7ef
rose-zoe <rose-zoe@protonmail.ch>
2022-03-30 16:22:57 +0100

    Add null test case

---
+++ src/editor/codemirror/dropdown.test.ts
@@ -0,0 +1,41 @@
+import { ChangeSet, EditorState, Text, Transaction } from "@codemirror/state";
+import { EditorView, ViewUpdate } from "@codemirror/view";
+import { dropdownPluginInternal } from "./dropdown";
+
+describe("dropdown", () => {
+  it("null case", () => {
+    const view = createView();
+    const plugin = new (dropdownPluginInternal(["foo","bar","baz"]))(view);
+
+    expect(plugin.decorations.size).toEqual(0); //Empty document should have no dropdowns
+
+    plugin.update(createViewUpdate(view, false, undefined));
+
+    expect(plugin.decorations.size).toEqual(0); //Empty document with no update should still have no dropdowns
+  });
+
+
+});
+
+const createView = (doc: Text = Text.of([""])): EditorView => {
+  return {
+    visibleRanges: [{ from: 0, to: doc.length - 1 }],
+    state: EditorState.create({ doc }),
+    dispatch: jest.fn(),
+  } as Partial<EditorView> as unknown as EditorView;
+};
+
+const createViewUpdate = (
+  view: EditorView,
+  docChanged: boolean,
+  transaction: Transaction | undefined
+): ViewUpdate => {
+  const state = transaction ? transaction.state || view.state : view.state;
+  return {
+    view: createView(state.doc),
+    state,
+    transactions: transaction ? [transaction] : [],
+    changes: transaction ? transaction.changes : ChangeSet.empty(0),
+    docChanged,
+  } as Partial<ViewUpdate> as unknown as any;
+};
