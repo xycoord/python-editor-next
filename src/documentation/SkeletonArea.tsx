5dc8d12db317f9a9992202264b4d3629cf4de1d6
Babissimo <alexandercharters@protonmail.com>
2022-04-26 22:25:53 +0000

    skele_menu_but_stylisitcally_consistent

--- src/documentation/SkeletonArea.tsx
+++ src/documentation/SkeletonArea.tsx
@@ -3,7 +3,7 @@
  *
  * SPDX-License-Identifier: MIT
  */
-import { Box, Text } from "@chakra-ui/layout";
+import { Box, List, ListItem, Text } from "@chakra-ui/layout";
 import { useIntl } from "react-intl";
 import HeadedScrollablePanel from "../common/HeadedScrollablePanel";
 import AreaHeading from "../common/AreaHeading";
@@ -23,28 +23,38 @@ const SkeletonArea = () => {
         />
       }
     >
-      <Text p={5} height="100%">
-        <Box height="100%">
-          <CodeEmbed code={"for i in range(0):\n   pass" }/>
+      <List flex="1 1 auto" m={3}>
+        <ListItem key={"for loop"}>
+          <Text  p={5}>
+            <CodeEmbed code={"for i in range(0):\n   pass" }/>
+          </Text>
           <Divider borderWidth="1px" />
-        </Box>
-        <Box height="100%">
-          <CodeEmbed code={"while False:\n   pass" }/>
+        </ListItem>
+        <ListItem key={"while loop"}>
+          <Text  p={5}>
+            <CodeEmbed code={"while False:\n   pass" }/>
+          </Text>
           <Divider borderWidth="1px" />
-        </Box>
-        <Box height="100%">
-          <CodeEmbed code={"if False:\n   pass" }/>
+        </ListItem>
+        <ListItem key={"if stmt"}>
+          <Text  p={5}>
+            <CodeEmbed code={"if False:\n   pass" }/>
+          </Text>
           <Divider borderWidth="1px" />
-        </Box>
-        <Box height="100%">
-          <CodeEmbed code={"if False:\n   pass\nelse:\n   pass" }/>
+        </ListItem>
+        <ListItem key={"ifel stmt"}>
+          <Text  p={5}>
+            <CodeEmbed code={"if False:\n   pass\nelse:\n   pass" }/>
+          </Text>
           <Divider borderWidth="1px" />
-        </Box>
-        <Box bg="gray.25" flex="0 0 auto" position="sticky" top="0" zIndex={1}>
-          <CodeEmbed code={"if False:\n   pass\nelif False:\n   pass\nelse:\n   pass" }/>
-        </Box>
-      </Text>
-      <Box height="100%" />
+        </ListItem>
+        <ListItem key={"ifelifel stmt"}>
+          <Text  p={5}>
+            <CodeEmbed code={"if False:\n   pass\nelif False:\n   pass\nelse:\n   pass" }/>
+          </Text>
+          <Divider borderWidth="1px"/>
+        </ListItem>
+      </List>
     </HeadedScrollablePanel>
   );
 };
